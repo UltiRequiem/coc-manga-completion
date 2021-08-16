bf9eb48d211afb036ed72b700aed13e06cd1b295
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-16 15:41:08 -0500

    :sparkles: Update

--- getAnimeTitlesFromWikipedia.js
+++ getAnimeTitlesFromWikipedia.js
@@ -1,28 +1,28 @@
 /* eslint-disable no-undef */
 // Wikipedia uses jquery
 
-let anime = '';
+let anime = ''
 
 $('#mw-pages li').each(() => {
-  anime += `${$(this).text()}\n`;
-});
+  anime += `${$(this).text()}\n`
+})
 
 function download(filename, text) {
-  const element = document.createElement('a');
+  const element = document.createElement('a')
   element.setAttribute(
     'href',
     `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`
-  );
-  element.setAttribute('download', filename);
+  )
+  element.setAttribute('download', filename)
 
-  element.style.display = 'none';
-  document.body.appendChild(element);
+  element.style.display = 'none'
+  document.body.appendChild(element)
 
-  element.click();
+  element.click()
 
-  document.body.removeChild(element);
+  document.body.removeChild(element)
 }
 
-download(`${$('#firstHeading').text()}.txt`, anime);
+download(`${$('#firstHeading').text()}.txt`, anime)
 
 // Credits: https://anime.stackexchange.com/questions/31426
