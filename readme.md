3b22c6f49ad1d182673ef5bc1dce207032da88d0
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-19 16:29:05 -0500

    :bookmark: v1.2.0

--- readme.md
+++ readme.md
@@ -22,10 +22,10 @@ Open an Issue, I will check it a soon as possible 👀
 If you want to hurry me up a bit
 [send me a tweet](https://twitter.com/UltiRequiem) 😆
 
-Consider [supporting me on Patreon](https://patreon.com/UltiRequiem) if you
-like my work 🙏
+Consider [supporting me on Patreon](https://patreon.com/UltiRequiem) if you like
+my work 🙏
 
-Don't forget to start the repo ⭐
+Don't forget to star the repo ⭐
 
 ## Licence
 
--- src/index.js
+++ src/index.js
@@ -1,47 +1,47 @@
-const { sources } = require('coc.nvim')
-const path = require('path')
-const fs = require('fs/promises')
+const { sources } = require("coc.nvim");
+const path = require("path");
+const fs = require("fs/promises");
 
-let words = []
+let words = [];
 
 exports.activate = async (context) => {
-  const file = path.resolve(__dirname, 'manga.txt')
+  const file = path.resolve(__dirname, "manga.txt");
 
-  fs.readFile(file, 'utf8', (err, content) => {
+  fs.readFile(file, "utf8", (err, content) => {
     if (err) {
-      return
+      return;
     }
-    words = content.split(/\n/)
-  })
+    words = content.split(/\n/);
+  });
 
   context.subscriptions.push(sources.createSource({
-    name: 'manga',
+    name: "manga",
     triggerCharacters: [],
-    async doComplete (opt) {
+    async doComplete(opt) {
       if (!opt.input) {
-        return null
+        return null;
       }
 
       if (!/^[A-Za-z]{1,}$/.test(opt.input)) {
-        return null
+        return null;
       }
 
-      const first = opt.input[0]
-      const list = words.filter((s) => s[0] === first.toLowerCase())
+      const first = opt.input[0];
+      const list = words.filter((s) => s[0] === first.toLowerCase());
 
-      const code = first.charCodeAt(0)
+      const code = first.charCodeAt(0);
 
-      const upperCase = code <= 90 && code >= 65
+      const upperCase = code <= 90 && code >= 65;
 
       return {
         items: list.map((text) => {
-          const word = upperCase ? text[0].toUpperCase() + text.slice(1) : text
+          const word = upperCase ? text[0].toUpperCase() + text.slice(1) : text;
           return {
             word,
-            menu: this.menu
-          }
-        })
-      }
-    }
-  }))
-}
+            menu: this.menu,
+          };
+        }),
+      };
+    },
+  }));
+};
